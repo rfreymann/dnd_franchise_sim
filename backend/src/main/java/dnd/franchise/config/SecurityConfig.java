574a176465fe26906d085fd175628adc59043a48
rfreymann <raphael.freymann@outlook.com>
2025-05-25 22:10:28 +0200

    add rafrey.com to cors

--- backend/src/main/java/dnd/franchise/config/SecurityConfig.java
+++ backend/src/main/java/dnd/franchise/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+    config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*", "https://staging.rafrey.com", "https://franchise.rafrey.com"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
